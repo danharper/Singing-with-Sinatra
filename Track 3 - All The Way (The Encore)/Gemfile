360b4fd8e18b2c6f08dea5aa364176eac0697041
Dan Harper <intouch@danharper.me>
2011-03-27 21:08:33 +0100

    Error handling, RSS etc.

---
+++ Track 3 - All The Way (The Encore)/Gemfile
@@ -0,0 +1,6 @@
+source "http://rubygems.org/"
+
+gem "sinatra"
+gem "datamapper"
+gem "rack-flash"
+gem "sinatra-redirect-with-flash"
--- Track 3 - All The Way (The Encore)/recall.rb
+++ Track 3 - All The Way (The Encore)/recall.rb
@@ -1,6 +1,14 @@
 require 'sinatra'
 require 'datamapper'
 require 'time'
+require 'rack-flash'
+require 'sinatra/redirect_with_flash'
+
+SITE_TITLE = "Recall"
+SITE_DESCRIPTION = "'cause you're too busy to remember"
+
+enable :sessions
+use Rack::Flash, :sweep => true
 
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/recall.db")
 
@@ -13,9 +21,6 @@ class Note
 	property :updated_at, DateTime
 end
 
-SITE_TITLE = "Recall"
-SITE_DESCRIPTION = "'cause you're too busy to remember"
-
 DataMapper.auto_upgrade!
 
 helpers do
@@ -23,19 +28,32 @@ helpers do
 	alias_method :h, :escape_html
 end
 
+
+# 
+# Application
+#
+
 get '/' do
 	@notes = Note.all :order => :id.desc
 	@title = 'All Notes'
+	if @notes.empty?
+		flash[:error] = 'No notes found. Add your first below.'
+	end 
 	erb :home
 end
 
 post '/' do
 	n = Note.new
-	n.content = params[:content]
-	n.created_at = Time.now
-	n.updated_at = Time.now
-	n.save
-	redirect '/'
+	n.attributes = {
+		:content => params[:content],
+		:created_at => Time.now,
+		:updated_at => Time.now
+	}
+	if n.save
+		redirect '/', :notice => 'Note created successfully.'
+	else
+		redirect '/', :error => 'Failed to save note.'
+	end
 end
 
 get '/rss.xml' do
@@ -46,34 +64,61 @@ end
 get '/:id' do
 	@note = Note.get params[:id]
 	@title = "Edit note ##{params[:id]}"
-	erb :edit
+	if @note
+		erb :edit
+	else
+		redirect '/', :error => "Can't find that note."
+	end
 end
 
 put '/:id' do
 	n = Note.get params[:id]
-	n.content = params[:content]
-	n.complete = params[:complete] ? 1 : 0
-	n.updated_at = Time.now
-	n.save
-	redirect '/'
+	unless n
+		redirect '/', :error => "Can't find that note."
+	end
+	n.attributes = {
+		:content => params[:content],
+		:complete => params[:complete] ? 1 : 0,
+		:updated_at => Time.now
+	}
+	if n.save
+		redirect '/', :notice => 'Note updated successfully.'
+	else
+		redirect '/', :error => 'Error updating note.'
+	end
 end
 
 get '/:id/delete' do
 	@note = Note.get params[:id]
 	@title = "Confirm deletion of note ##{params[:id]}"
-	erb :delete
+	if @note
+		erb :edit
+	else
+		redirect '/', :error => "Can't find that note."
+	end
 end
 
 delete '/:id' do
 	n = Note.get params[:id]
-	n.destroy
-	redirect '/'
+	if n.destroy
+		redirect '/', :notice => 'Note deleted successfully.'
+	else
+		redirect '/', :error => 'Error deleting note.'
+	end
 end
 
 get '/:id/complete' do
 	n = Note.get params[:id]
-	n.complete = n.complete ? 0 : 1 # flip it
-	n.updated_at = Time.now
-	n.save
-	redirect '/'
+	unless n
+		redirect '/', :error => "Can't find that note."
+	end
+	n.attributes = {
+		:complete => n.complete ? 0 : 1, # flip it
+		:updated_at => Time.now
+	}
+	if n.save
+		redirect '/', :notice => 'Note marked as complete.'
+	else
+		redirect '/', :error => 'Error marking note as complete.'
+	end
 end
